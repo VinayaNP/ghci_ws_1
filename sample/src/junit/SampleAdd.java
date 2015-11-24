8f0b37246800dadce5819e9bb268154346740e94
sindhu <sirajesh@cisco.com>
2015-11-24 12:22:47 +0530

    test
    Change-Id: I9531478a2a72407713feab83763eef3f07af7e27

--- sample/src/junit/SampleAdd.java
+++ sample/src/junit/SampleAdd.java
@@ -3,6 +3,6 @@ package junit;
 public class SampleAdd {
   public int add(int x, int y) 
    { 
-     System.out.println("Addition is :" + (x + y));
+     System.out.println("Addition is- " + (x + y));
      return x + y; }
  }
