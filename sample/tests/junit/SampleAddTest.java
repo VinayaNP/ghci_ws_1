eeae884ddeb60d97a11281d789687823d2746f95
vinaya <vinaya.parvatikar@gmail.com>
2015-12-01 14:59:26 +0530

    Updated junit test
    Change-Id: I81dea01232c25ad0bbf98ee897cf48878cedabff

--- sample/tests/junit/SampleAddTest.java
+++ sample/tests/junit/SampleAddTest.java
@@ -15,7 +15,7 @@ public class SampleAddTest {
   @Test 
   public void testAdd() 
   { Assert.assertEquals(5, sampleClass.add(3, 2));
-    Assert.assertEquals(7, sampleClass.add(4, 2));
+    Assert.assertEquals(6, sampleClass.add(4, 2));
   }
  
   
